60eb34fd80e01bcdabc544de4ffb14746812ba4a
khush <khushgandhisince1998@gmail.com>
2022-07-31 14:53:59 +0530

    created the header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import "./Header.css";
+
+export default function Header(){
+return (<div className="header">
+    <div className="content">Home</div>
+    <div className="content" style={{marginLeft:'35rem'}}>My Chat App</div>
+</div>);
+}
